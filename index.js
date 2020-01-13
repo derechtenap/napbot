f96df0592064fc7bf4691b83f4306e4350cd4f3a
derechtenap <50623315+derechtenap@users.noreply.github.com>
2020-01-13 20:55:00 +0100

    Request geht

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-const Discord = require("discord.js");
-const token = require("./token.json");
+const Discord = require('discord.js');
+const token = require('./token.json');
+const config = require('./config.json');
+
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+var username;
+var ausgabe;
+
+const HTTP = new XMLHttpRequest();
+const url = 'https://aoe2.net/leaderboard/aoe2de/rm-1v1?search[value]=Mond2001'; // Später value=username
+
 const client = new Discord.Client();
 
-client.once("ready", () => {
-    console.log("Ready!");
+client.once('ready', () => {
+    //console.log('\n' + client.user.tag + ' wurde geladen! napbot ist bereit\nDer Bot ist mit diesen Servern verbunden: ');
+    //client.guilds.forEach((guild) => {
+    //    console.log('- ' + guild.name)
+    //})
+    client.user.setActivity('Wieder da!', {
+        type: 'WATCHING'
+    })
+});
+
+client.on('message', message => {
+
+    if (message.content === config.prefix + 'ladder') {
+        HTTP.open("GET", url);
+        HTTP.send();
+
+        HTTP.onreadystatechange = (e) => {
+           console.log(HTTP.responseText);
+        }
+    }
 });
 
 client.login(token.id);
