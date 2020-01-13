2bc3efabb41b2b9d944d12daa755c1e255fd2186
derechtenap <50623315+derechtenap@users.noreply.github.com>
2020-01-13 19:03:34 +0100

    Bot Setup

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+const token = require("./token.json");
+const client = new Discord.Client();
+
+client.once("ready", () => {
+    console.log("Ready!");
+});
+
+client.login(token.id);
