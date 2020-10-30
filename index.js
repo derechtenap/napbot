af64100e232301dda845fb48709b2f1a96cd78bc
derechtenap <50623315+derechtenap@users.noreply.github.com>
2020-10-30 16:24:51 +0100

    Update index.js
    Some minor changes and the bot can now display his current version

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 'use strict';
 
+// DiscordJS und Fetch
 const Discord = require('discord.js');
+const fetch = require('node-fetch');
+
 const client = new Discord.Client();
 
-const MISC = require('./misc.js');
+const misc = require('./misc.js');
 
 // Config and Token
-const CONFIG = require('./config.json');
+const { prefix, language, commandList } = require('./config.json');
 const TOKEN = require('./token.json');
-const PREFIX = CONFIG.prefix;
 
+// Client on ready -------------------------------------------------------
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
   // Set bot status
-  client.user.setActivity(`.version - ${MISC.data.version()}`,  {
+  client.user.setActivity(`.version - ${misc.data.version()}`,  {
       type: 'WATCHING'
   })
-    .then(presence => console.info(`Activity set to ${presence.activities[0].name}`))
     .catch(console.error);
 });
 
 client.on('message', msg => {
-    const ARGS = msg.content.slice(PREFIX.length).split(' ');
-    const COMMAND = ARGS.shift().toLowerCase();
+    const ARGS = msg.content.slice(prefix.length).split(' ');
+    const command = ARGS.shift().toLowerCase();
 
     // Ignore messages without prefix and bot messages...
-    if(!msg.content.startsWith(PREFIX) || msg.author.bot) return; 
+    if(!msg.content.startsWith(prefix) || msg.author.bot) return; 
 
-    if(COMMAND === 'version') {
-        msg.channel.send('`' + MISC.data.version() + '`');
+    if(commandList.includes('version') && command === 'version') {
+        msg.channel.send('`' + misc.data.version() + '`');
     }
 });
 
