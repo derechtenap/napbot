107ef73d556d6a3fd64dad3eae1ff0a3038bd761
derechtenap <50623315+derechtenap@users.noreply.github.com>
2021-05-15 22:19:49 +0200

    Update misc.js

--- misc.js
+++ misc.js
@@ -6,16 +6,6 @@ const {
 
 // Misc methods ----------------------------------------------------------
 var methods = {
-    profiles: function displayProfiles() {
-        let names = "";
-
-        for (let i = 0; i < profiles.array.length; i++) {
-            names += `${profiles.array[i].name} ` +
-                `(${profiles.array[i].steam_id})\n`;
-        }
-        return names;
-    },
-
     version: function displayVersion() {
         return version;
     }
